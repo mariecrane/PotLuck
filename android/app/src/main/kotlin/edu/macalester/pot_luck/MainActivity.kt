e52293d7585875c47225a706e3307f38e95a9812
Marie Crane <mcrane@macalester.edu>
2019-09-30 15:34:49 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/edu/macalester/pot_luck/MainActivity.kt
@@ -0,0 +1,13 @@
+package edu.macalester.pot_luck
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
