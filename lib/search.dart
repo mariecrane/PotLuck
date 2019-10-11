6c187c759188a2724de0cbfb37fd01e4bc68b736
Preston Locke <locke_preston@yahoo.com>
2019-10-11 15:09:49 -0500

    Added getRecipeInfo and recipe data models

--- lib/search.dart
+++ lib/search.dart
@@ -4,18 +4,19 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-// Encodes all the data that is returned by our recipe API interface
-// (Can be expanded later)
+/// Encodes all the data that is returned by our recipe API interface
+/// (Can be expanded later)
 class SearchResult {
+  final int id;
   final String recipeName;
   final int missedIngredients;
   final int matchedIngredients;
 
-  SearchResult(
+  SearchResult(this.id,
       {this.recipeName, this.missedIngredients, this.matchedIngredients});
 }
 
-// Encodes the type and data of events coming from our search UI
+/// Encodes the type and data of events coming from our search UI
 abstract class SearchEvent {}
 
 class Submit extends SearchEvent {}
@@ -25,7 +26,7 @@ class QueryChange extends SearchEvent {
   QueryChange(this.searchString);
 }
 
-// Encodes the status and data of results returned from our recipe API interface
+/// Encodes the status and data of results returned from our recipe API interface
 abstract class SearchState {}
 
 class InitialState extends SearchState {}
@@ -37,9 +38,10 @@ class SearchSuccessful extends SearchState {
   SearchSuccessful(this.results);
 }
 
-// Connects our business logic with our UI code in an extensible way
+/// Connects our business logic with our UI code in an extensible way
 class SearchBloc extends Bloc<SearchEvent, SearchState> {
   String _searchString;
+  String _lastSearch;
 
   @override
   SearchState get initialState => InitialState();
@@ -53,6 +55,9 @@ class SearchBloc extends Bloc<SearchEvent, SearchState> {
     }
     // Commence a search if our user hit submit
     if (event is Submit) {
+      // Don't submit if the search didn't actually change
+      if (_searchString == _lastSearch) return;
+
       if (_searchString != "") {
         // Let our UI know we're currently performing a search
         yield SearchInProgress();
@@ -60,7 +65,8 @@ class SearchBloc extends Bloc<SearchEvent, SearchState> {
         // This is a horrible workaround but somehow the only working one to
         // get our SearchInProgress state to actually send
         // Hopefully unneeded when we put in real API calls?
-        await Future.delayed(Duration(milliseconds: 100));
+        // Update: seems unnecessary now, but for safety, we'll keep this around
+        // await Future.delayed(Duration(milliseconds: 100));
 
         // When our search returns results, pass them to the UI
         var results =
@@ -80,22 +86,40 @@ class RecipeSearch {
 
   final String _apiKey = "";
 
+  bool get useMetricUnits {
+    // TODO: Add logic to determine whether we should use US or Metric units
+    // (Could also maybe fetch this from app settings in the future...)
+    return false;
+  }
+
+  /// Builds the full URL for an image of an ingredient
+  String ingredientImageUrl(String fileName) {
+    // TODO: Add some way to pick the image size? (Choices are 100, 250, 500)
+    String size = "250x250";
+    return "https://spoonacular.com/cdn/ingredients_$size/$fileName";
+  }
+
+  /// Adds GET parameters to a url, also adding the API key automatically
   String addParamsToUrl(String url, Map<String, dynamic> params) {
+    // Always add API key as the first parameter in the URL
+    url = "$url?apiKey=$_apiKey&";
+
     if (params == null || params.length == 0) return url;
 
-    url = "$url?apiKey=$_apiKey&";
     params.forEach((key, value) {
       // Insert key-value pairs into url parameter format
       url = "$url$key=$value&";
     });
 
     // Remove unnecessary trailing ampersand
-    return url.substring(0, url.length - 2);
+    return url.substring(0, url.length - 1);
   }
 
-  // Fetches recipe results
+  /// Fetches recipe results asynchronously
   Future<List<SearchResult>> getRecipeResults(String searchString) async {
-    if (searchString.indexOf(",") == -1) searchString+=","; // fixes weird one search term bug //TODO: investigate further
+    // TODO: investigate further
+    // Fixes weird one search term bug
+    if (searchString.indexOf(",") == -1) searchString += ",";
     var url = addParamsToUrl(
       "https://api.spoonacular.com/recipes/findByIngredients",
       <String, dynamic>{"ingredients": searchString},
@@ -108,6 +132,7 @@ class RecipeSearch {
 
     data.forEach((result) {
       resultList.add(SearchResult(
+        result["id"],
         recipeName: result["title"],
         matchedIngredients: result["usedIngredientCount"],
         missedIngredients: result["missedIngredientCount"],
@@ -115,16 +140,94 @@ class RecipeSearch {
     });
 
     return resultList;
-
-//    if (searchString == "ham,cheese") {
-//      // TODO: Don't forget to remove artificial delay
-//      await Future.delayed(Duration(seconds: 1));
-//      return <SearchResult>[
-//        SearchResult(recipeName: "Ham & Cheese Sandwich"),
-//        SearchResult(recipeName: "Snack Crackers"),
-//        SearchResult(recipeName: "Gourmet Crescent Rolls"),
-//      ];
-//    }
-//    return List<SearchResult>();
   }
+
+  /// Fetches detailed info for a single recipe asynchronously
+  Future<RecipeInfo> getRecipeInfo(SearchResult recipe) async {
+    // We call addParamsToURL to add the api key to the URL
+    var url = addParamsToUrl(
+        "https://api.spoonacular.com/recipes/${recipe.id}/information", {});
+    debugPrint(url);
+
+    // Get the API response, formatted as a JSON object
+    var response = await http.get(url);
+    var data = convert.jsonDecode(response.body);
+
+    // Parse out the list of required ingredients
+    var ingredients = List<RecipeIngredient>();
+    bool useMetric = useMetricUnits;
+    data["extendedIngredients"].forEach((ingredient) {
+      // Get either US or Metric quantity values
+      var measure = useMetric
+          ? ingredient["measures"]["metric"]
+          : ingredient["measures"]["us"];
+
+      // Create a RecipeIngredient object with all of our parsed information
+      ingredients.add(RecipeIngredient(
+        id: ingredient["id"],
+        name: ingredient["name"],
+        amount: measure["amount"],
+        unit: measure["unitShort"],
+        imageUrl: ingredientImageUrl(ingredient["image"]),
+      ));
+    });
+
+    // TODO: Handle multiple sets of instructions (prerequisite ingredients)
+    // Parse out the list of steps
+    var steps = List<RecipeStep>();
+    data["analyzedInstructions"][0]["steps"].forEach((instruction) {
+      steps.add(RecipeStep(
+        instructionsText: instruction["step"],
+      ));
+    });
+
+    return RecipeInfo(
+      id: data["id"],
+      title: data["title"],
+      imageUrl: data["image"],
+      sourceUrl: data["sourceUrl"],
+      creditsText: data["creditsText"],
+      ingredients: ingredients,
+      steps: steps,
+    );
+  }
+}
+
+/// Models the available info about a single recipe
+class RecipeInfo {
+  final int id;
+  final String title;
+  final String imageUrl;
+  final String sourceUrl;
+  final String creditsText;
+  final List<RecipeIngredient> ingredients;
+  final List<RecipeStep> steps;
+
+  RecipeInfo(
+      {this.id,
+      this.title,
+      this.imageUrl,
+      this.sourceUrl,
+      this.creditsText,
+      this.ingredients,
+      this.steps});
+}
+
+/// Models a single step of instructions for a single recipe
+class RecipeStep {
+  final String instructionsText;
+
+  RecipeStep({this.instructionsText});
+}
+
+/// Models a single ingredient necessary for a specific recipe, including the
+/// amount needed
+class RecipeIngredient {
+  final int id;
+  final String name;
+  final double amount;
+  final String unit;
+  final String imageUrl;
+
+  RecipeIngredient({this.id, this.name, this.amount, this.unit, this.imageUrl});
 }
