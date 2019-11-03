ae49ae52287cab4e1a95e622f2e6c93086fa208c
mariecrane <mcrane@macalester.edu>
2019-11-03 15:51:04 -0600

    Added firebase to iOS
    Many files changed within ios folder. Followed directions on firebase to add an ios app.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,12 +2,26 @@ import UIKit
 import Flutter
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//This is the new code that firebase gave me to put here
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+      [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+    return true
   }
 }
+//This is the old code
+//@objc class AppDelegate: FlutterAppDelegate {
+//  override func application(
+//    _ application: UIApplication,
+//    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+//  ) -> Bool {
+//    GeneratedPluginRegistrant.register(with: self)
+//    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//  }
+//}
+
