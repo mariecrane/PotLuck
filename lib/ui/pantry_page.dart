c2853b02700596e8682dd9b66dd362f771ee51b9
Shouayee Vue <svue1@macalester.edu>
2019-11-13 09:23:05 -0600

    I am pushing for Marie to run emulator for me on the changes i make to correct the error

--- lib/ui/pantry_page.dart
+++ lib/ui/pantry_page.dart
@@ -3,57 +3,50 @@ import 'package:flutter/material.dart';
 class PantryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        automaticallyImplyLeading: true,
-        title: Padding(
-          // Adds some padding around our TextField
-          padding: const EdgeInsets.symmetric(
-            vertical: 5.0,
-          ),
-          child: TextField(
-            // Type of "Done" button to show on keyboard
-            textInputAction: TextInputAction.search,
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              // Shows when TextField is empty
-              hintText: "Add Ingredients to Pantry...",
-            ),
-            onSubmitted: (value) {},
-            onChanged: (value) {},
-          ),
-        ),
-        leading: IconButton(
-          icon: Icon(Icons.add, color: Theme
-              .of(context)
-              .primaryColor),
-        ),
-      ),
-      body: ListView.builder(
-      ),
-    );
-  }
+      return ListView(
+        key: PageStorageKey<String>("pantry_page"),
+        children: ListTile.divideTiles(
+          context: context,
+          tiles: [
+            new Container(),
+            Card(),
+            Padding(),
+            Padding(),
+          ],
+        ).toList(),
+      );
+    }
 
   static Widget buildAppBar(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.white,
-      elevation: 1.0,
-      title: Text(
-        "Your Pantry",
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          color: Theme
-              .of(context)
-              .primaryColor,
-          fontSize: 28,
+      automaticallyImplyLeading: true,
+      title: Padding(
+        // Adds some padding around our TextField
+        padding: const EdgeInsets.symmetric(
+          vertical: 5.0,
         ),
+        child: TextField(
+          // Type of "Done" button to show on keyboard
+          textInputAction: TextInputAction.search,
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            // Shows when TextField is empty
+            hintText: "Add Ingredients to Pantry...",
+          ),
+          onSubmitted: (value) {},
+          onChanged: (value) {},
+        ),
+      ),
+      leading: IconButton(
+        icon: Icon(Icons.add, color: Theme
+            .of(context)
+            .primaryColor),
       ),
     );
   }
 
-  static Widget buildFloatingActionButton
-      (BuildContext context) {
+  static Widget buildFloatingActionButton(BuildContext context) {
     return null;
   }
 }
