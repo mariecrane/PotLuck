3e639ec4432fb72efaa2f1ceb43aeada7fc06ff6
Shouayee Vue <svue1@macalester.edu>
2019-11-13 09:01:25 -0600

    I have removed dropdown and replaced with expansion tile.

--- lib/ui/pantry_page.dart
+++ lib/ui/pantry_page.dart
@@ -3,207 +3,117 @@ import 'package:flutter/material.dart';
 class PantryPage extends StatelessWidget {
   @override
   static Widget buildAppBar(BuildContext context) {
-    return AppBar(
-      backgroundColor: Colors.white,
-      automaticallyImplyLeading: true,
-      title: Padding(
-        // Adds some padding around our TextField
-        padding: const EdgeInsets.symmetric(
-          vertical: 5.0,
-        ),
-        child: TextField(
-          // Type of "Done" button to show on keyboard
-          textInputAction: TextInputAction.search,
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            // Shows when TextField is empty
-            hintText: "Add Ingredients to Pantry...",
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: true,
+        title: Padding(
+          // Adds some padding around our TextField
+          padding: const EdgeInsets.symmetric(
+            vertical: 5.0,
           ),
-          onSubmitted: (value) {},
-          onChanged: (value) {},
+          child: TextField(
+            // Type of "Done" button to show on keyboard
+            textInputAction: TextInputAction.search,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              // Shows when TextField is empty
+              hintText: "Add Ingredients to Pantry...",
+            ),
+            onSubmitted: (value) {},
+            onChanged: (value) {},
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.add, color: Theme
+              .of(context)
+              .primaryColor),
         ),
       ),
-      leading: IconButton(
-        icon: Icon(Icons.add, color: Theme
-            .of(context)
-            .primaryColor),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) =>
+            EntryItem(data[index]),
+        itemCount: data.length,
       ),
     );
   }
 
-  String dropdownValue1 = 'All Ingredients';
-  String dropdownValue2 = 'Proteins';
-  String dropdownValue3 = 'Vegetables';
-  String dropdownValue4 = 'Fruits';
-  String dropdownValue5 = 'Other';
+  @override
   Widget build(BuildContext context) {
-    return ListView(
-              children: ListTile.divideTiles(
-              context: context,
-              tiles: [
-                DropdownButton<String>(
-                  value: dropdownValue1,
-                  icon: Icon(Icons.arrow_downward, color: Theme
-                      .of(context)
-                      .primaryColor),
-                  iconSize: 24,
-                  elevation: 0,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
-                      color: Theme
-                          .of(context)
-                          .primaryColor
-                  ),
-                  underline: Container(
-                    height: 4,
-                    color: Theme
-                        .of(context)
-                        .primaryColor,
-                  ),
-                  onChanged: (String newValue) {
-                    dropdownValue1 = newValue;
-                  },
-                  items: <String>[
-                    'All Ingredients',
-                    'Chicken',
-                    'Beef',
-                    'Lettuce',
-                    'Peas',
-                    'Orange',
-                    'Apple',
-                    'Flour',
-                    'Cheetos',]
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  })
-                      .toList(),
-                ),
-                DropdownButton<String>(
-                  value: dropdownValue2,
-                  icon: Icon(Icons.arrow_downward, color: Colors.pink),
-                  iconSize: 24,
-                  elevation: 0,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
-                      color: Colors.pink
-                  ),
-                  underline: Container(
-                    height: 4,
-                    color: Colors.pink,
-                  ),
-                  onChanged: (String newValue) {
-                    dropdownValue2 = newValue;
-                  },
-                  items: <String>[
-                    'Proteins',
-                    'Chicken',
-                    'Beef']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  })
-                      .toList(),
-                ),
-                DropdownButton<String>(
-                  value: dropdownValue3,
-                  icon: Icon(Icons.arrow_downward, color: Colors.green),
-                  iconSize: 24,
-                  elevation: 0,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
-                      color: Colors.green
-                  ),
-                  underline: Container(
-                    height: 4,
-                    color: Colors.green,
-                  ),
-                  onChanged: (String newValue) {
-                    dropdownValue3 = newValue;
-                  },
-                  items: <String>[
-                    'Vegetables',
-                    'Lettuce',
-                    'Peas']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  })
-                      .toList(),
-                ),
-                DropdownButton<String>(
-                  value: dropdownValue4,
-                  icon: Icon(Icons.arrow_downward, color: Colors.purple),
-                  iconSize: 24,
-                  elevation: 0,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
-                      color: Colors.purple
-                  ),
-                  underline: Container(
-                    height: 4,
-                    color: Colors.purple,
-                  ),
-                  onChanged: (String newValue) {
-                    dropdownValue4 = newValue;
-                  },
-                  items: <String>[
-                    'Fruits',
-                    'Apple',
-                    'Orange']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  })
-                      .toList(),
-                ),
-                DropdownButton<String>(
-                  value: dropdownValue5,
-                  icon: Icon(Icons.arrow_downward, color: Colors.black),
-                  iconSize: 24,
-                  elevation: 0,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
-                      color: Colors.black
-                  ),
-                  underline: Container(
-                    height: 4,
-                    color: Colors.black
-                  ),
-                  onChanged: (String newValue) {
-                    dropdownValue5 = newValue;
-                  },
-                  items: <String>[
-                    'Other',
-                    'Flour',
-                    'Cheetos']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  })
-                      .toList(),
-                ),
-              ],
-            ).toList(),
-          );
+   // TODO: implement build
+    return null;
   }
 
-  static Widget buildFloatingActionButton(BuildContext context) {
+  static Widget buildFloatingActionButton
+      (BuildContext context) {
     return null;
+  }
+}
+
+class Entry {
+  Entry(this.title, [this.children = const <Entry>[]]);
+
+  final String title;
+  final List<Entry> children;
+}
+
+final List<Entry> data = <Entry>[
+  Entry(
+    'Chapter A',
+    <Entry>[
+      Entry(
+        'Section A0',
+        <Entry>[
+          Entry('Item A0.1'),
+          Entry('Item A0.2'),
+          Entry('Item A0.3'),
+        ],
+      ),
+      Entry('Section A1'),
+      Entry('Section A2'),
+    ],
+  ),
+  Entry(
+    'Chapter B',
+    <Entry>[
+      Entry('Section B0'),
+      Entry('Section B1'),
+    ],
+  ),
+  Entry(
+    'Chapter C',
+    <Entry>[
+      Entry('Section C0'),
+      Entry('Section C1'),
+      Entry(
+        'Section C2',
+        <Entry>[
+          Entry('Item C2.0'),
+          Entry('Item C2.1'),
+          Entry('Item C2.2'),
+          Entry('Item C2.3'),
+        ],
+      ),
+    ],
+  ),
+];
+
+class EntryItem extends StatelessWidget {
+  const EntryItem(this.entry);
+
+  final Entry entry;
+
+  Widget _buildTiles(Entry root) {
+    if (root.children.isEmpty) return ListTile(title: Text(root.title));
+    return ExpansionTile(
+      key: PageStorageKey<Entry>(root),
+      title: Text(root.title),
+      children: root.children.map(_buildTiles).toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildTiles(entry);
   }
 }
