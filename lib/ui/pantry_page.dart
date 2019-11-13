447e733dbc184d6b106b1e0ae57f04db7672dd72
Shouayee Vue <svue1@macalester.edu>
2019-11-13 09:11:19 -0600

    I made changes so that Pantry might now have an error

--- lib/ui/pantry_page.dart
+++ lib/ui/pantry_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class PantryPage extends StatelessWidget {
   @override
-  static Widget buildAppBar(BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -31,89 +31,29 @@ class PantryPage extends StatelessWidget {
         ),
       ),
       body: ListView.builder(
-        itemBuilder: (BuildContext context, int index) =>
-            EntryItem(data[index]),
-        itemCount: data.length,
       ),
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-   // TODO: implement build
-    return null;
+  static Widget buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 1.0,
+      title: Text(
+        "Your Pantry",
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Theme
+              .of(context)
+              .primaryColor,
+          fontSize: 28,
+        ),
+      ),
+    );
   }
 
   static Widget buildFloatingActionButton
       (BuildContext context) {
     return null;
-  }
-}
-
-class Entry {
-  Entry(this.title, [this.children = const <Entry>[]]);
-
-  final String title;
-  final List<Entry> children;
-}
-
-final List<Entry> data = <Entry>[
-  Entry(
-    'Chapter A',
-    <Entry>[
-      Entry(
-        'Section A0',
-        <Entry>[
-          Entry('Item A0.1'),
-          Entry('Item A0.2'),
-          Entry('Item A0.3'),
-        ],
-      ),
-      Entry('Section A1'),
-      Entry('Section A2'),
-    ],
-  ),
-  Entry(
-    'Chapter B',
-    <Entry>[
-      Entry('Section B0'),
-      Entry('Section B1'),
-    ],
-  ),
-  Entry(
-    'Chapter C',
-    <Entry>[
-      Entry('Section C0'),
-      Entry('Section C1'),
-      Entry(
-        'Section C2',
-        <Entry>[
-          Entry('Item C2.0'),
-          Entry('Item C2.1'),
-          Entry('Item C2.2'),
-          Entry('Item C2.3'),
-        ],
-      ),
-    ],
-  ),
-];
-
-class EntryItem extends StatelessWidget {
-  const EntryItem(this.entry);
-
-  final Entry entry;
-
-  Widget _buildTiles(Entry root) {
-    if (root.children.isEmpty) return ListTile(title: Text(root.title));
-    return ExpansionTile(
-      key: PageStorageKey<Entry>(root),
-      title: Text(root.title),
-      children: root.children.map(_buildTiles).toList(),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return _buildTiles(entry);
   }
 }
