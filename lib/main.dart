0118aa7e3352993e861c563d60789226abfaabaf
Preston Locke <locke_preston@yahoo.com>
2019-11-15 09:57:40 -0600

    Fixed primary theme color swatch

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,19 @@ import 'package:pot_luck/ui/nav.dart';
 
 void main() => runApp(MyApp());
 
-Map<int, Color> color = {
-  50: Color.fromRGBO(226, 132, 19, .1),
-  100: Color.fromRGBO(226, 132, 19, .2),
-  200: Color.fromRGBO(226, 132, 19, .3),
-  300: Color.fromRGBO(226, 132, 19, .4),
-  400: Color.fromRGBO(226, 132, 19, .5),
-  500: Color.fromRGBO(226, 132, 19, .6),
-  600: Color.fromRGBO(226, 132, 19, .7),
-  700: Color.fromRGBO(226, 132, 19, .8),
-  800: Color.fromRGBO(226, 132, 19, .9),
-  900: Color.fromRGBO(226, 132, 19, 1),
+const _primaryColor = <int, Color>{
+  50: Color.fromRGBO(242, 180, 105, 1),
+  100: Color.fromRGBO(240, 171, 86, 1),
+  200: Color.fromRGBO(239, 162, 67, 1),
+  300: Color.fromRGBO(237, 152, 49, 1),
+  400: Color.fromRGBO(235, 143, 30, 1),
+  500: Color.fromRGBO(226, 132, 19, 1),
+  600: Color.fromRGBO(206, 122, 18, 1),
+  700: Color.fromRGBO(188, 111, 16, 1),
+  800: Color.fromRGBO(169, 100, 15, 1),
+  900: Color.fromRGBO(150, 88, 13, 1),
 };
-MaterialColor myColor = MaterialColor(0xffe28413, color);
+const _primarySwatch = MaterialColor(0xffe28413, _primaryColor);
 
 class MyApp extends StatelessWidget {
   @override
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "PotLuck",
       theme: ThemeData(
-        primarySwatch: myColor,
+        primarySwatch: _primarySwatch,
       ),
       // Update based on authentication state
       home: MultiBlocProvider(
