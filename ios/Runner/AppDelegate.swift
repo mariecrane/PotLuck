7fac9f59fad3ada9a459cc6a9744be20e5470c66
mariecrane <mcrane@macalester.edu>
2019-11-17 12:27:09 -0600

    Fixed iOS bugs!
    Added pods to podfile and changed AppDelegate file to fix the iOS bugs.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,26 +3,14 @@ import Flutter
 import Firebase
 
 @UIApplicationMain
-//This is the new code that firebase gave me to put here
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-  var window: UIWindow?
-
-  func application(_ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions:
-    [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
     FirebaseApp.configure()
-    return true
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
-//This is the old code
-//@objc class AppDelegate: FlutterAppDelegate {
-//  override func application(
-//    _ application: UIApplication,
-//    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-//  ) -> Bool {
-//    GeneratedPluginRegistrant.register(with: self)
-//    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-//  }
-//}
 
